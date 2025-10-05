39d8cea8cde96b859b796aa8ea51f68667133a52
ChelseaOliviaSitorus <chelseasitorus98@smp.belajar>
2025-10-06 06:41:07 +0700

    Tugas Mini Kalkulator Chelsea Olivia Sitorus

---
+++ projek/1-miniCalculator.js
@@ -0,0 +1,37 @@
+let a = 10
+let b = 5
+
+const tambah = () => {
+    console.log(`pertambahan dari a tambah b adalah ${a} + ${b} = ${a + b}`);
+    
+}
+tambah()
+
+const kurang = () => {
+    console.log(`pengurangan dari a kurang b adalah ${a} - ${b} = ${a - b }`);
+    
+}
+kurang()
+
+const kali = () => {
+    console.log(`perkalian dari a kali b adalah ${a} x ${b} = ${a * b}`);
+}
+kali()
+
+const bagi = () => {
+    console.log(`pembagian dari a bagi b adalah ${a} : ${b} = ${a / b}`);
+    
+}
+bagi()
+
+const pangkat = () => {
+    console.log(`perpangkatan dari a pangkat b adalah ${a} ^ ${b} ${a ** b}`);
+    
+}
+pangkat()
+
+const modulus = () => {
+    console.log(`modulus dari a modulus b adalah ${a} % ${b} ${a % b}`);
+    
+}
+modulus()
